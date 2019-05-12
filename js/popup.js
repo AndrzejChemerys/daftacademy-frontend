75710b14890d4d05f669620558a783eb0130e56f
AndriiChemerys <andriichemerys@gmail.com>
2019-05-12 21:57:26 +0200

    finalizing the project

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 let btn = document.querySelectorAll(".button");
 let arrBtn = Array.from(btn);
-let modal=document.querySelector(".modal");
+let modal = document.querySelector(".modal");
 
 for (i = 0; i < arrBtn.length; ++i) {
     arrBtn[i].addEventListener("click", () => {
---
+++ readme.md
@@ -0,0 +1,21 @@
+- - -
+### Daftcode intership test 2019
+- - -
+
+**1.** Layout i RWD.
+
+> Project 1 link:
+
+- - -
+
+**2.** Modal.
+
+> Project 2 link:
+
+- - -
+
+**3.** Filtrowanie listy.
+
+> Project 3 link:
+
+- - -
